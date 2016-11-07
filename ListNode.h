24333e8c4f3d3a17b8ae585ea7c5c6fd5348d807
ChrisNath <chrisnath20@gmail.com>
2016-11-07 03:12:24 +0200

    Add files via upload

---
+++ ListNode.h
@@ -0,0 +1,66 @@
+#ifndef LISTNODE_H
+#define LISTNODE_H
+#include <iostream>
+#include <string>
+#include <stdint.h> 
+
+
+
+class list_node {
+	uint32_t* neighbor; //the ids of the neighbor nodes
+	uint32_t* edgeProperty; //property for each edge
+	list_node* nextListNode; //
+	uint32_t n;
+	uint32_t noOfneigbors;
+
+public:
+	list_node(const int n)
+	{
+		neighbor = new uint32_t(n);
+		edgeProperty = new uint32_t(n);
+		nextListNode = NULL;
+		noOfneigbors = 0;
+	}
+
+	~list_node()
+	{
+		delete neighbor;
+		delete edgeProperty;
+		list_node* temp = nextListNode;
+		while (nextListNode != NULL)
+		{
+			temp = nextListNode->nextListNode;
+			delete nextListNode;
+			nextListNode = temp;
+		}
+	}
+
+	list_node* getNextListNode()
+	{
+		return nextListNode;
+	}
+
+	void setNext(list_node* next)
+	{
+		nextListNode = next;
+	}
+	bool isFull()
+	{
+		if (noOfneigbors >= n)
+		{
+			return true;
+		}
+		return false;
+	}
+	int addNeighbor(uint32_t nodeId)
+	{
+		neighbor[noOfneigbors] = nodeId;
+		noOfneigbors++;
+		return EXIT_SUCCESS;
+	}
+	
+};
+
+#endif
+
+
